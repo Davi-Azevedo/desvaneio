e7de89590428f51881970d26b1737df9078727ff
Davi-Azevedo <45953247+Davi-Azevedo@users.noreply.github.com>
2022-05-10 10:27:28 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+    $paginas= ['HOME'=>'Pagina inicial aqui', 'CONTA'=>'Iformações da sua conta', 'MISSÕES'=>'Nenhuma missão disponivel no momento', 'CONQUISTAS'=>'Aqui estão sua conquistas', 'FAQ'=>' Tire suas duvidas aqui', 'SOBRE'=> 'Nossas informações aqui'];
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>site teste</title>
+    <style type='text/css'>
+        *{
+            margin:0;
+            padding:0;
+            box-sizing: 0;
+
+        }
+        header{
+            background-color: #069;
+            padding:8px 10px;
+            text-align: center22222i;
+        }
+
+    </style>
